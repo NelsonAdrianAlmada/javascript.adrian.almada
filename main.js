222fd104002915a7fb1f18fe4cbb929296b1593f
Adrian Almada <adrianpianoalmada@outlook.com>
2022-05-07 12:24:56 -0300

    modificando

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-let nombreUsuario = prompt("ingresa tu nombre");
-let(mensaje + nombreUsuario);
-alert(mensaje+nombreUsuario);
+let nombre= prompt("Queres saber tu promedio de la carrera full stack? presiona enter");
+
+let curso1 = prompt("Cual fue tu nota en desarrollo web?");
+let curso2 = prompt("En Javascript?");
+let curso3 = prompt("En React js?");
+let curso4 = prompt("En programacion Backend?");
+let promedio = (parseFloat(curso1)+parseFloat(curso2)+parseFloat(curso3)+parseFloat(curso4))/4
+;
+alert("Su promedio es: "+promedio)
